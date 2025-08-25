1be46fa10c8f1f98fad3f6f9c27f5f283745b9f7
IrinaQA423 <irina-koshina@mail.ru>
2025-08-25 16:49:05 +0800

    Update models.py

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -33,11 +33,11 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='entities', verbose_name="Покемон")
     appeared_at = models.DateTimeField(default=timezone.now, verbose_name="Время появления")
     disappeared_at = models.DateTimeField(default=timezone.now, verbose_name="Время исчезновения")
-    level = models.IntegerField(default=1, verbose_name="Уровень")
-    health = models.IntegerField(default=100, verbose_name="Здоровье")
-    strength = models.IntegerField(default=10, verbose_name="Сила")
-    defence = models.IntegerField(default=10, verbose_name="Защита")
-    stamina = models.IntegerField(default=10, verbose_name="Выносливость")
+    level = models.IntegerField(default=1, verbose_name="Уровень", null=True, blank=True)
+    health = models.IntegerField(default=100, verbose_name="Здоровье", null=True, blank=True)
+    strength = models.IntegerField(default=10, verbose_name="Сила", null=True, blank=True)
+    defence = models.IntegerField(default=10, verbose_name="Защита", null=True, blank=True)
+    stamina = models.IntegerField(default=10, verbose_name="Выносливость", null=True, blank=True)
 
     def __str__(self):
         return f"{self.pokemon.title} (уровень: {self.level})"
